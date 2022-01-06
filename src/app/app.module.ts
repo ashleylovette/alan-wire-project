0871880ab478d70c241379723b32da53d9219cd1
ashleylovette <ashleylovette15@gmail.com>
2022-01-06 12:56:47 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainGridComponent } from './main-grid/main-grid.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainGridComponent
+    MainGridComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
