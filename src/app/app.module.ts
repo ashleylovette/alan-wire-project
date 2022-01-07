734f85db28c33cf5e860784651bdba239deb0662
betta7635 <betta7635@yahoo.com>
2022-01-06 21:21:55 -0600

    left-sidebar
    Mostly completed BASIC DRAFT of left-sidebar component. Some styling issues remaining and transitions with main content area to be tested.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftSidebarComponent
   ],
   imports: [
     BrowserModule
