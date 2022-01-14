5b51e04e1456577393705ee49192113cfe5607a3
RSandage <90716032+RSandage@users.noreply.github.com>
2022-01-14 10:03:48 -0600

    Dashboard Item Fixes

--- src/app/main-grid/dashboard-item/dashboard-item.component.ts
+++ src/app/main-grid/dashboard-item/dashboard-item.component.ts
@@ -1,14 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< Updated upstream
-
-@Component({
-  selector: 'app-dashboard-item',
-  templateUrl: '.main-grid/dashboard-item/dashboard-item.component.html',
-  styleUrls: '.main-grid/dashboard-item/dashboard-item.component.css',
-})
-export class DashboardItemComponent implements OnInit {
-  constructor() {}
-=======
 import { DashboardService } from 'src/app/services/dashboard.service';
 
 @Component({
@@ -21,7 +11,6 @@ export class DashboardItemComponent implements OnInit {
   imgPath: string;
 
   constructor(dashboardService: DashboardService) {}
->>>>>>> Stashed changes
 
   ngOnInit(): void {}
 }
