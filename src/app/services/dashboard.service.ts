3a9502a37cc22ca7903755b274594fdc5560ebff
betta7635 <betta7635@yahoo.com>
2022-01-14 09:06:00 -0600

    Merge branch abc'main' of https://github.com/ashleylovette/alan-wire-project

---
+++ src/app/services/dashboard.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { DashboardItem } from "../main-grid/dashboard-item.model";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DashboardService {
+  private dashboard: DashboardItem[] = [
+    new DashboardItem('Test 1', '3', 'https://cdn.corporatefinanceinstitute.com/assets/line-graph.jpg'),
+    new DashboardItem('Test 2', '4', 'https://cdn.corporatefinanceinstitute.com/assets/bar-and-line-graph-1.jpg'),
+    new DashboardItem('Test Table', '2', 'https://blogs.sas.com/content/graphicallyspeaking/files/2014/12/graphTable-300x225.png')
+  ]
+
+  constructor() {}
+
+  getDashboard() {
+    return this.dashboard.slice();
+  }
+
+  addItem(newItem: DashboardItem) {
+    this.dashboard.push(newItem);
+    this.getDashboard();
+  }
+
+  deleteItem(index: number) {
+    this.dashboard.splice(index, 1);
+    this.getDashboard();
+  }
+
+}
