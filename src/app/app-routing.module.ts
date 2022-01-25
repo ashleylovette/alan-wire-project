7c067d1231eb4541408df669c049f51d53c33f56
ashleylovette <ashleylovette15@gmail.com>
2022-01-25 10:31:50 -0600

    dashboard selected

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { DashboardStartComponent } from "./dashboard-start/dashboard-start.component";
+import { MainGridComponent } from "./main-grid/main-grid.component";
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardStartComponent, children: [
+    { path: ':id', component: MainGridComponent }
+  ]}
+
+]
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
