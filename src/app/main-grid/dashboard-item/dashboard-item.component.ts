817dcfbc462f28d02c815b0cf66c00be47f98fb3
RSandage <90716032+RSandage@users.noreply.github.com>
2022-01-29 14:51:17 -0600

    Added ability to delete dashitems from current dashboard.

--- src/app/main-grid/dashboard-item/dashboard-item.component.ts
+++ src/app/main-grid/dashboard-item/dashboard-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { DashboardService } from 'src/app/services/dashboard.service';
 import { DashboardItem } from './dashboard-item.model';
 
 @Component({
@@ -11,8 +12,12 @@ export class DashboardItemComponent implements OnInit {
     @Input() dbItem: DashboardItem;
     @Input() index: number;
 
-  constructor() {}
+  constructor(private dashService: DashboardService) {}
 
   ngOnInit() {
+  }
+
+  onDelete() {
+    this.dashService.deleteDashItem(this.index);
   }
 }
