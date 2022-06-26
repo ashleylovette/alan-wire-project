eb9bf159a8a444725a9f9be30dfb503bc35ac13d
Jessie Woods <jessieallyn@gmail.com>
2022-06-26 14:40:16 -0500

    WIP routing and auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,16 @@ import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { AuthGuard } from './auth/auth.guard';
 
+// const routes: Routes = [
+//   {path: '', redirectTo: '/custom-dashboard', pathMatch: 'full' },
+//   {path: 'custom-dashboard', component: AppComponent, canActivate: [AuthGuard] },
+//   {path: 'login', component: AuthComponent},
+//   {path: '**', redirectTo: '/custom-dashboard'}
+// ];
+
 const routes: Routes = [
   {path: '', redirectTo: '/custom-dashboard', pathMatch: 'full' },
-  {path: 'custom-dashboard', component: AppComponent, canActivate: [AuthGuard] },
+  {path: 'custom-dashboard', component: AppComponent },
   {path: 'login', component: AuthComponent},
   {path: '**', redirectTo: '/custom-dashboard'}
 ];
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+// import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +11,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  userEmail: string ="";
+  error: string = null;
+  isLoginMode = true;
 
-  constructor() { }
+  // constructor(private authService: AuthService, private router: Router, private http: HttpClient ) { }
+  constructor( private router: Router, private http: HttpClient ) { }
 
   ngOnInit(): void {
   }
+//commented out onLogin function because correct endpoints haven't been put in
+  // onLogin(form: NgForm) {
+  //   //if there isn't a valid form, the function does not run
+  //   if (!form.valid) {
+  //     return;
+  //   }
+  //   //declaring variables
+  //   const email = form.value.email;
+  //   const password = form.value.password;
 
-}
+  //   let authObs: Observable<any> =
+  //     this.authService.login(email, password);
+
+  //   authObs.subscribe(
+  //     resData => {
+  //       console.log(resData);
+  //       this.router.navigate(['/home']);
+  //    },
+  //    errorMessage => {
+  //      console.log(errorMessage);
+  //      this.error = errorMessage;
+  //    }
+  //   );
+
+  //   form.reset();
+  //   }
+//the previous way of doing this didn't require next: error: complete:
+//https://rxjs.dev/deprecations/subscribe-arguments check here for
+//the reasoning behind the deprecation of the usual way of .subscribe
+//this uses the AuthService login method & subscribes to it
+//it sends the login request
+
+  // if (this.isLoginMode) {
+  //   authObs = this.authService.login(email,password);
+  // }
+
+  // }
+
+    // if (this.isLoginMode) {
+    //   this.authService.login(email, password).subscribe({
+    //     next: (resData) => console.log(resData),
+    //     error: (errorMessage) => {
+    //       console.log(errorMessage);
+    //       this.error = errorMessage;
+    //     },
+    //     complete: () => console.info('complete')
+    //   })
+    // }
+  }
