af391c968810d83b22d8027bee9afd4e6078a77c
RSandage <90716032+RSandage@users.noreply.github.com>
2022-06-28 12:20:49 -0500

    Changed models to match backend

--- src/app/main-grid/dashboard.model.ts
+++ src/app/main-grid/dashboard.model.ts
@@ -1,11 +1,13 @@
 import { DashboardItem } from './dashboard-item/dashboard-item.model';
 
 export class Dashboard {
-  public name: string;
+  public name?: string;
+  public user_id: number;
   public items?: DashboardItem[];
 
-  constructor(name: string, items?: []) {
+  constructor(name: string, user_id: number, items?: []) {
     this.name = name;
+    this.user_id = user_id;
     this.items = items;
   }
 }
