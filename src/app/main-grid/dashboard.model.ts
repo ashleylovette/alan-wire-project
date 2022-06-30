246d4a73a260c8f8be4937579610ff8d97961c04
RSandage <90716032+RSandage@users.noreply.github.com>
2022-06-30 12:37:17 -0500

    Update dashboard.model.ts

--- src/app/main-grid/dashboard.model.ts
+++ src/app/main-grid/dashboard.model.ts
@@ -2,12 +2,10 @@ import { DashboardItem } from './dashboard-item/dashboard-item.model';
 
 export class Dashboard {
   public name?: string;
-  public user_id: number;
   public items?: DashboardItem[];
 
   constructor(name: string, user_id: number, items?: []) {
     this.name = name;
-    this.user_id = user_id;
     this.items = items;
   }
 }
